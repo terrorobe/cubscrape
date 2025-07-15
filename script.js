b9f469823069c8970e155a406209759cb3598694
Michael Renner <terrorobe@github.com>
2025-07-15 23:46:07 +0200

    path path

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ async function loadSQLiteDatabase() {
         locateFile: file => `https://sql.js.org/dist/${file}`
     });
     
-    const response = await fetch('/data/games.db');
+    const response = await fetch('data/games.db');
     if (!response.ok) {
         throw new Error(`Failed to load database: ${response.status}`);
     }
